3d4f38279fd29dc90010fdc3fd915243bdc1c726
dubrewer <dustinbrewer92@yahoo.com>
2019-02-15 08:01:38 -0500

    barplots and anovas for guilds

--- GuildsData.R
+++ GuildsData.R
@@ -26,21 +26,49 @@ Omnivores.SE <- tapply(Guilds$Omn_16, Guilds$mortality.class, sem)
 
 Insectivores.SE <- tapply(Guilds$Ins_29, Guilds$mortality.class, sem)
 
-#Make barplots (ADJUST, at least bars and ylim needs to be adjusted)
-OmnivorePlot <- barplot(Omnivores.Mean, ylim = c(0, round(max(Omnivores.Mean), digits = 0)),
+#Omnivore bar plot
+OmnivorePlot <- barplot(Omnivores.Mean, ylim = c(0, round(1.5*max(Omnivores.Mean), digits = 0)),
                pch = 15, cex = 1.25, las = 1, cex.lab = 1.4, cex.axis = 1.25,
                xlab = "mortality class",
                ylab = "mean site abundance",
                names.arg = c("low", "medium", "high A", "high B"))
 
-arrows(x0 = bp1, y0 = Omnivores.Mean, y1 = Omnivores.Mean - Omnivores.SE, angle = 90,
+arrows(x0 = OmnivorePlot, y0 = Omnivores.Mean, y1 = Omnivores.Mean - Omnivores.SE, angle = 90,
        length = 0.1, lwd = 1)
-arrows(x0 = bp1, y0 = Omnivores.Mean , y1 = Omnivores.Mean + Omnivores.SE, angle = 90,
+arrows(x0 = OmnivorePlot, y0 = Omnivores.Mean , y1 = Omnivores.Mean + Omnivores.SE, angle = 90,
        length = 0.1, lwd = 1)
+
+#Insectivore bar plot
+InsectivorePlot <- barplot(Insectivores.Mean, ylim = c(0, round(1.5*max(Insectivores.Mean), digits = 0)),
+                        pch = 15, cex = 1.25, las = 1, cex.lab = 1.4, cex.axis = 1.25,
+                        xlab = "mortality class",
+                        ylab = "mean site abundance",
+                        names.arg = c("low", "medium", "high A", "high B"))
+
+arrows(x0 = InsectivorePlot, y0 = Insectivores.Mean, y1 = Insectivores.Mean - Insectivores.SE, angle = 90,
+       length = 0.1, lwd = 1)
+arrows(x0 = InsectivorePlot, y0 = Insectivores.Mean , y1 = Insectivores.Mean + Insectivores.SE, angle = 90,
+       length = 0.1, lwd = 1)
+
+
+
 
 #Try to get the omnivore and insectivore bars on same plot
 
-#Run Anovas
+#Run Omnivore Anova
+
+OmnivoreAnova <- aov(Guilds$Omn_16~Guilds$mortality.class, data = Guilds)
+OmnivoreAnova
+
+summary(OmnivoreAnova)
+TukeyHSD(OmnivoreAnova)
+
+#Run Insectivore ANOVA
+
+InsectivoreAnova <- aov(Guilds$Ins_29~Guilds$mortality.class, data = Guilds)
+
+summary(InsectivoreAnova)
+TukeyHSD(OmnivoreAnova)
 
 
 
